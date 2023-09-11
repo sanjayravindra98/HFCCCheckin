90bd8b6f95466cd52c0430e87acf8d0a641ce784
Sanjay Ravindra <63814176+sanjayravindra98@users.noreply.github.com>
2023-09-11 19:13:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCMjFNxcITUhsQVB_4rVFMQsKdqMPQ425w",
+    authDomain: "hfcccheckin.firebaseapp.com",
+    projectId: "hfcccheckin",
+    storageBucket: "hfcccheckin.appspot.com",
+    messagingSenderId: "82135060851",
+    appId: "1:82135060851:web:9f4d8a51910fe33163b506"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
