5c3a2741d1c21e932573e7b5a732fd018d121632
Sanjay Ravindra <63814176+sanjayravindra98@users.noreply.github.com>
2023-09-11 19:02:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Get a reference to the button element by its ID
-const redirectButton = document.getElementById("redirectButton");
-
-// Add a click event listener to the button
-redirectButton.addEventListener("click", function() {
-    // Specify the relative path to the target HTML file
-    const targetPage = "./index.html"; // Replace with the actual filename
-
-    // Redirect the user to the target HTML page
-    window.location.href = targetPage;
-});
+//
