92c917c23a806c3242c94455d3565865d4690289
Sanjay Ravindra <63814176+sanjayravindra98@users.noreply.github.com>
2023-09-11 20:22:45 -0400

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -13,23 +13,3 @@ firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the Firebase Authentication object
 const auth = firebase.auth();
-
-// Function to check if the user is authenticated as "dummy@example.com"
-function checkAuthentication() {
-    auth.onAuthStateChanged((user) => {
-        if (user && user.email === "dummy@example.com") {
-            // User is authenticated as "dummy@example.com"
-            // You can allow access to checkin.html here
-            console.log("User is authenticated as dummy@example.com");
-            window.location.href = "./checkin.html"; // Redirect to checkin.html
-        } else {
-            // User is not authenticated or is not "dummy@example.com"
-            // You can redirect them to the login page or display an error message
-            console.log("User is not authenticated or is not dummy@example.com");
-            window.location.href = "./index.html"; // Redirect to login.html or another page
-        }
-    });
-}
-
-// Call the function to check authentication status
-checkAuthentication();
