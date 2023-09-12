b093e6735fe73d8296159ae3db4a6921eb699480
Sanjay Ravindra <63814176+sanjayravindra98@users.noreply.github.com>
2023-09-11 20:26:28 -0400

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -13,3 +13,14 @@ firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the Firebase Authentication object
 const auth = firebase.auth();
+
+// checkin.js or your authentication control script
+auth.onAuthStateChanged((user) => {
+    if (!user) {
+        // User is not authenticated; redirect to login page
+        window.location.href = "./index.html"; // Redirect to login
+    } else {
+        // User is authenticated; you can proceed with displaying the checkin page
+    }
+});
+
