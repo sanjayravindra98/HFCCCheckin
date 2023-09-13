fb6005ca48a93912d7cc0d8a97b7f35700bc3547
Sanjay Ravindra <63814176+sanjayravindra98@users.noreply.github.com>
2023-09-13 00:31:11 -0400

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -110,18 +110,6 @@ const selectedStudents = [];
 function handleStudentButtonClick(studentData, studentButton) {
   const isPresent = studentData.present;
   const isSelected = selectedStudents.includes(studentData);
-  const isCurrentlySelectingGreen = selectedStudents.some(student => student.present);
-  const isCurrentlySelectingBlue = selectedStudents.every(student => !student.present);
-
-  if (isCurrentlySelectingGreen && isPresent) {
-    // Currently selecting green students, cannot select another green student
-    return;
-  }
-
-  if (isCurrentlySelectingBlue && !isPresent) {
-    // Currently selecting blue students, cannot select another blue student
-    return;
-  }
 
   if (isPresent && isSelected) {
     // Student is already marked present and selected, deselect it
@@ -159,8 +147,6 @@ function handleStudentButtonClick(studentData, studentButton) {
   confirmButton.style.display = canConfirm ? 'block' : 'none';
   undoButton.style.display = canUndo ? 'block' : 'none';
 }
-
-
 
 // Function to handle the click event for the "Undo" button
 function handleUndoButtonClick() {
